64a53bd2cb2cc254269fa4e0b8e8b70d55dc4f31
Johnathan Bressette <bressetteprogramming@gmail.com>
2020-08-22 18:59:55 -0400

    Add check for rapidjson folder in copy script

--- copy-to-ns3.sh
+++ copy-to-ns3.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Configure the following values
-RAPIDJSON_FOLDER=~/rapidjson
-NS3_FOLDER=~/workspace/ns-allinone-3.25/ns-3.25
+# rapidjson can be installed using "sudo apt install rapidjson-dev" on debian distributions
+# Set the value of the environment variable NS3_HOME to set the ns3 folder correctly
+RAPIDJSON_FOLDER=/usr
+NS3_FOLDER=$NS3_HOME
 
 # Do not change
-mkdir $NS3_FOLDER/rapidjson
+
+# If the rapidjson folder does not exist create the directory
+if [ ! -d "$NS3_FOLDER/rapidjson" ]; then
+	mkdir $NS3_FOLDER/rapidjson
+fi
 cp  -r $RAPIDJSON_FOLDER/include/rapidjson/* $NS3_FOLDER/rapidjson/
 cp  src/applications/model/* $NS3_FOLDER/src/applications/model/
 cp  src/applications/helper/* $NS3_FOLDER/src/applications/helper/
